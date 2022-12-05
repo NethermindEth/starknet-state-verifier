61c229354906bba6145b04dc00fd6195b14440f0
Fawad <153737+FawadHa1der@users.noreply.github.com>
2022-12-05 15:27:15 -0500

    initial commit

---
+++ contracts/StarknetVerifier.sol
@@ -0,0 +1,186 @@
+pragma solidity >=0.6.2 <0.9.0;
+pragma experimental ABIEncoderV2;
+import "./EllipticCurve.sol";
+import "./PedersenHashNaive.sol";
+import "hardhat/console.sol";
+
+contract StarknetVerifier {
+    enum NodeType {
+        BINARY,
+        EDGE
+    }
+
+    struct BinaryProof {
+        uint256 leftHash;
+        uint256 rightHash;
+    }
+
+    struct EdgeProof {
+        uint256 childHash;
+        uint256 path;
+        int256 length;
+    }
+
+    struct StarknetProof {
+        NodeType nodeType;
+        BinaryProof binaryProof;
+        EdgeProof edgeProof;
+    }
+
+    function empty_method(uint256 rootHash) public view {
+        console.log("empty_method rootHash: %s", rootHash);
+    }
+
+    function uint256ToBinaryArray(uint256 input)
+        internal
+        pure
+        returns (uint256[] memory)
+    {
+        uint256 integer = input;
+        uint256[] memory array = new uint256[](251);
+        uint256 i = 0;
+        while (integer > 0) {
+            uint256 bit = integer % 2;
+            uint256 quotient = integer / 2;
+            array[i] = bit;
+            integer = quotient;
+            i++;
+        }
+        return array;
+    }
+
+    function comparePaths(
+        uint256[] memory path1Array,
+        uint256[] memory path2Array,
+        uint256 start,
+        uint256 length
+    ) internal pure returns (bool) {
+        // uint256[] memory path1Array = uint256ToBinaryArray(path1);
+        // uint256[] memory path2Array = uint256ToBinaryArray(path2);
+        for (uint256 i = start; i < start + length; i++) {
+            if (path1Array[i] != path2Array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function hashForSingleProofNode(StarknetProof memory proof)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 hashvalue = 0;
+        if (proof.nodeType == NodeType.BINARY) {
+            hashvalue = PedersenHashNaive.hash(
+                proof.binaryProof.leftHash,
+                proof.binaryProof.rightHash
+            );
+        } else {
+            hashvalue =
+                (PedersenHashNaive.hash(
+                    proof.edgeProof.childHash,
+                    proof.edgeProof.path
+                ) + uint256(proof.edgeProof.length)) %
+                3618502788666131213697322783095070105623107215331596699973092056135872020481;
+        }
+        console.log("hashForSingleProofNode hashvalue: %s", hashvalue);
+        return hashvalue;
+    }
+
+    function fromBinaryArrayToUint(uint256[] memory input)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 n = 0;
+
+        for (int256 i = 250; i >= 0; i--) {
+            n *= 2;
+            uint256 index = uint256(i);
+            if (input[index] == 1) {
+                n += 1;
+            } else {
+                // revert on malformed input
+                require(input[index] == 0);
+            }
+        }
+        console.log("fromBinaryArrayToUint n: %s", n);
+        return n;
+    }
+
+    function verify_proof(
+        uint256 rootHash,
+        uint256 path,
+        uint256 value,
+        StarknetProof[] calldata proofArray
+    ) public view returns (bool) {
+        uint256 expectedHash = rootHash;
+        // uint256 i = 0;
+        uint256[] memory path_bits = uint256ToBinaryArray(path);
+        uint256 originalValue = fromBinaryArrayToUint(path_bits);
+        console.log("verify_proof originalValue: %s", originalValue);
+        require(originalValue == path, "path and paths_bits are not equal");
+        uint256 path_bits_index = 0;
+
+        for (uint256 x = 0; x < 251; x++) {
+            console.log("verify_proof path_bits[%s]: %s", x, path_bits[x]);
+        }
+
+        // 1 is right
+        // 0 is left
+
+        console.log("rootHash: %s", rootHash);
+        for (uint256 i = 0; i < proofArray.length; i++) {
+            StarknetProof memory proof = proofArray[i];
+            if (expectedHash != hashForSingleProofNode(proof)) {
+                console.log("we failed");
+                return false;
+            } else {
+                console.log("yaay we passed");
+            }
+            if (proof.nodeType == NodeType.BINARY) {
+                bool isRight = (path_bits[path_bits_index] == 1);
+                if (isRight == true) {
+                    expectedHash = proof.binaryProof.rightHash;
+                } else {
+                    expectedHash = proof.binaryProof.leftHash;
+                }
+                path_bits_index++;
+                console.log(
+                    "Binary",
+                    proof.binaryProof.leftHash,
+                    proof.binaryProof.rightHash
+                );
+            } else {
+                console.log("Edge");
+                if (
+                    false ==
+                    comparePaths(
+                        path_bits,
+                        uint256ToBinaryArray(proof.edgeProof.path),
+                        path_bits_index,
+                        uint256(proof.edgeProof.length)
+                    )
+                ) {
+                    return false;
+                } else {
+                    console.log("we passed the comparePaths");
+                    expectedHash = proof.edgeProof.childHash;
+                    path_bits_index += uint256(proof.edgeProof.length);
+                }
+                console.log(
+                    "Edge",
+                    proof.edgeProof.path,
+                    proof.edgeProof.childHash
+                );
+            }
+        }
+
+        if (expectedHash == value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ test/index.ts
@@ -0,0 +1,134 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import fs from "fs";
+import path from "path";
+import { StarknetVerifier } from '../typechain/StarknetVerifier';
+import { StarknetVerifier__factory } from "../typechain/factories/StarknetVerifier__factory";
+import { PedersenHashNaive } from "../typechain";
+import {
+  EventFilter,
+  Signer,
+  BigNumber,
+  BigNumberish,
+  PopulatedTransaction,
+  BaseContract,
+  ContractTransaction,
+  CallOverrides,
+} from "ethers";
+
+interface MyBinaryProof {
+  leftHash: BigNumberish;
+  rightHash: BigNumberish;
+}
+
+interface MyEdgeProof {
+  childHash: BigNumberish;
+  path: BigNumberish;
+  length: BigNumberish;
+}
+
+interface MyStarknetProof {
+  nodeType: BigNumberish;
+  binaryProof: MyBinaryProof;
+  edgeProof: MyEdgeProof;
+}
+
+
+describe("Verify", function () {
+  it("it should verify the proof", async function () {
+    var jsonFile = "./sampleProof.json";
+    console.log("Reading file: " + jsonFile);
+    var filepath = path.resolve(__dirname, jsonFile)
+    console.log("Filepath: " + filepath);
+
+    let myproofs: MyStarknetProof[] = [];
+    var originalParse = JSON.parse(fs.readFileSync(filepath, 'utf8'))
+    originalParse.result.contract_proof.forEach((element: any) => {
+      if (element.Binary != undefined) {
+        myproofs.push({
+          nodeType: 0,
+          binaryProof: {
+            leftHash: element.Binary.left,
+            rightHash: element.Binary.right
+          },
+          edgeProof: {
+            childHash: 0,
+            path: 0,
+            length: 0
+          }
+        })
+      }
+      else if (element.Edge != undefined) {
+        myproofs.push({
+          nodeType: 1,
+          binaryProof: {
+            leftHash: 0,
+            rightHash: 0
+          },
+          edgeProof: {
+            childHash: element.Edge.child,
+            path: element.Edge.path.value,
+            length: element.Edge.path.len
+          }
+        });
+      }
+    });
+
+
+    // var myproofs = JSON.parse(fs.readFileSync(filepath, 'utf8'), (key, value) => {
+    //   console.log("Key: " + key + " Value: " + value);
+    //   if (key == "Binary") {
+    //     return {
+    //       nodeType: 0,
+    //       binaryProof: {
+    //         leftHash: value.left_hash,
+    //         rightHash: value.right_hash
+    //       },
+    //       edgeProof: {
+    //         childHash: 0,
+    //         path: 0,
+    //         length: 0
+    //       }
+    //     };
+    //   }
+    //   else if (key == "Edge") {
+    //     return {
+    //       nodeType: 1,
+    //       binaryProof: {
+    //         leftHash: 0,
+    //         rightHash: 0
+    //       },
+    //       edgeProof: {
+    //         childHash: value.child_hash,
+    //         path: value.path,
+    //         length: value.length
+    //       }
+    //     };
+    //   }
+    // });
+
+    console.log("Proofs: " + myproofs.toString());
+
+    const PedersenLib = await ethers.getContractFactory("PedersenHashNaive");
+    const pedersenLib = await PedersenLib.deploy();
+    await pedersenLib.deployed();
+
+    const StarknetVerifier = await ethers.getContractFactory("StarknetVerifier", {
+      libraries: {
+        PedersenHashNaive: pedersenLib.address,
+      }
+    },
+    );
+
+    const proofverifier = await StarknetVerifier.deploy() as StarknetVerifier;
+    await proofverifier.deployed();
+
+    console.log("Deployed to: ", proofverifier.address);
+
+    await proofverifier.empty_method("0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37");
+
+    const result = await proofverifier.verify_proof("0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37", "0x206f38f7e4f15e87567361213c28f235cccdaa1d7fd34c9db1dfe9489c6a091", "0x64233179314709baca174fce33d3691638260a7c5569b74a8efd30998753c9f", myproofs);
+    console.log("Result: " + result);
+
+  });
+});
