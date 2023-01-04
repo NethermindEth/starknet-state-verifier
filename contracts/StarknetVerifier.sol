fdbc022736a743fc29045ce9743a45a462c472c9
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-04 10:39:39 -0500

    remove storage var value expectation, the proof contains the value, no need to be passed in + return the storage var value after verificaion

--- contracts/StarknetVerifier.sol
+++ contracts/StarknetVerifier.sol
@@ -18,7 +18,6 @@ contract StarknetVerifier {
         uint256 contractStateRoot;
         uint256 contractAddress;
         uint256 storageVarAddress;
-        uint256 storageVarValue;
         uint256 classHash;
         uint256 hashVersion;
         uint256 nonce;
@@ -41,10 +40,41 @@ contract StarknetVerifier {
     }
 
     PedersenHash pedersen;
+    address starknetCoreContractAddress =
+        0xde29d060D45901Fb19ED6C6e959EB22d8626708e; // TODO pointing to address on goerli testnet, needs to be removed
 
-    constructor(address pedersenAddress) public {
+    constructor(
+        address pedersenAddress /*, address _starknetCoreContractAddress*/
+    ) public {
         pedersen = PedersenHash(pedersenAddress);
+        // starknetCoreContractAddress = _starknetCoreContractAddress;
     }
+
+    // // This is the signature of the stateBlockNumber function
+    // bytes4 constant STATE_BLOCK_NUMBER_SIGNATURE =
+    //     bytes4(keccak256("stateBlockNumber()"));
+
+    // // This is the signature of the stateBlockNumber function
+    // bytes4 constant STATE_BLOCK_ROOT_SIGNATURE =
+    //     bytes4(keccak256("stateRoot()"));
+
+    // // This function will call the stateBlockNumber function of the contract
+    // function callStateBlockNumber() public view returns (int256) {
+    //     // Call the contract and retrieve the result
+    //     return
+    //         address(starknetCoreContractAddress).call(
+    //             STATE_BLOCK_NUMBER_SIGNATURE
+    //         );
+    // }
+
+    // // This function will call the stateBlockNumber function of the contract
+    // function callStateBlockRoot() public view returns (uint256) {
+    //     // Call the contract and retrieve the result
+    //     return
+    //         address(starknetCoreContractAddress).call(
+    //             STATE_BLOCK_ROOT_SIGNATURE
+    //         );
+    // }
 
     function hashForSingleProofNode(StarknetProof memory proof)
         public
@@ -70,22 +100,22 @@ contract StarknetVerifier {
     // Computes the index of the highest bit set in 'number'.
     // Returns the highest bit set as an 'uint'.
     // Requires that 'number != 0'.
-    function highestBitSet(uint256 number)
-        public
-        pure
-        returns (uint256 highest)
-    {
-        require(number != 0);
-        uint256 val = number;
-        uint256 ONE = uint256(1);
+    // function highestBitSet(uint256 number)
+    //     public
+    //     pure
+    //     returns (uint256 highest)
+    // {
+    //     require(number != 0);
+    //     uint256 val = number;
+    //     uint256 ONE = uint256(1);
 
-        for (uint256 i = 128; i >= 1; i >>= 1) {
-            if (val & (((ONE << i) - 1) << i) != 0) {
-                highest += i;
-                val >>= i;
-            }
-        }
-    }
+    //     for (uint256 i = 128; i >= 1; i >>= 1) {
+    //         if (val & (((ONE << i) - 1) << i) != 0) {
+    //             highest += i;
+    //             val >>= i;
+    //         }
+    //     }
+    // }
 
     // state_hash = H(H(H(class_hash, contract_root), contract_nonce), RESERVED)
     function stateHash(
@@ -94,8 +124,6 @@ contract StarknetVerifier {
         uint256 nonce,
         uint256 hashVersion
     ) public view returns (uint256) {
-        // uint256 contractRootClassHash = ;
-        // uint256 rootClassHashWithNonce = ;
         uint256 stateHash = hash(
             hash(hash(classHash, contractStateRoot), nonce),
             hashVersion
@@ -124,86 +152,73 @@ contract StarknetVerifier {
         return b;
     }
 
-    function verifyCompleteProof(
+    function verifiedStorageValue(
         ContractData calldata contractData,
         StarknetProof[] calldata contractProofArray,
         StarknetProof[] calldata storageProofArray
-    ) public view returns (bool) {
+    ) public view returns (uint256 value) {
+        console.log("contractData.stateRoot", contractData.stateRoot);
         uint256 stateHash = stateHash(
             contractData.classHash,
             contractData.contractStateRoot,
             contractData.nonce,
             contractData.hashVersion
         );
-        bool isStorageProofValid = verify_proof(
+        uint256 storageVarValue = verifyProof(
             contractData.contractStateRoot,
             contractData.storageVarAddress,
-            contractData.storageVarValue,
             storageProofArray
         );
 
-        if (isStorageProofValid == false) {
-            return false;
-        }
-
-        bool isContractProofValid = verify_proof(
+        uint256 expectedStateHash = verifyProof(
             contractData.stateRoot,
             contractData.contractAddress,
-            stateHash,
             contractProofArray
         );
 
-        return isContractProofValid;
+        require(stateHash == expectedStateHash, "State hash is invalid");
+        return storageVarValue;
     }
 
-    function verify_proof(
+    function verifyProof(
         uint256 rootHash,
         uint256 path,
-        uint256 value,
         StarknetProof[] calldata proofArray
-    ) public view returns (bool) {
+    ) public view returns (uint256 value) {
         uint256 expectedHash = rootHash;
-        uint256 path_bits_index = highestBitSet(path);
+        int256 pathBitIndex = 250; // start from the highest bit index
 
         bool isRight = true;
         for (uint256 i = 0; i < proofArray.length; i++) {
-            StarknetProof memory proof = proofArray[i];
-            if (expectedHash != hashForSingleProofNode(proof)) {
-                return false;
-            }
-            if (proof.nodeType == NodeType.BINARY) {
-                isRight = ((path >> path_bits_index) & 1) == 1;
-                path = path & ~(1 << path_bits_index); // setting/clearing the bit as move through the path
-                if (isRight == true) {
-                    expectedHash = proof.binaryProof.rightHash;
-                } else {
-                    expectedHash = proof.binaryProof.leftHash;
+            if (pathBitIndex >= 0) {
+                StarknetProof memory proof = proofArray[i];
+                if (expectedHash != hashForSingleProofNode(proof)) {
+                    revert("Proof is invalid");
                 }
-                path_bits_index--;
-                // console.log(
-                //     "Binary",
-                //     proof.binaryProof.leftHash,
-                //     proof.binaryProof.rightHash
-                // );
-            } else {
-                // console.log("Edge");
-                // console.log("proof.edgeProof.path: %s", proof.edgeProof.path);
-                if (path != proof.edgeProof.path) {
-                    return false;
+                if (proof.nodeType == NodeType.BINARY) {
+                    isRight = ((path >> uint256(pathBitIndex)) & 1) == 1;
+                    // path = path & ~(1 << uint256(pathBitIndex)); // setting/clearing the bit as move through the path
+                    if (isRight == true) {
+                        expectedHash = proof.binaryProof.rightHash;
+                    } else {
+                        expectedHash = proof.binaryProof.leftHash;
+                    }
+                    pathBitIndex--;
                 } else {
-                    uint256 length = proof.edgeProof.length;
+                    // if (path != proof.edgeProof.path) {
+                    //     revert("Proof is invalid");
+                    // }
                     expectedHash = proof.edgeProof.childHash;
-                    path_bits_index -= uint256(
-                        highestBitSet(proof.edgeProof.path)
-                    );
+                    int256 edgePathLength = int256(proof.edgeProof.length);
+                    pathBitIndex -= edgePathLength;
+                    console.log("pathBitIndex", uint256(pathBitIndex));
                 }
             }
         }
-
-        if (expectedHash == value) {
-            return true;
-        } else {
-            return false;
+        // if the loop is complete and pathBitIndex is equal to -1 otherwise the proof is invalid
+        if (pathBitIndex == -1) {
+            return expectedHash;
         }
+        revert("Proof is invalid");
     }
 }
