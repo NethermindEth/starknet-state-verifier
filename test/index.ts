1826c93169ca4ffda8305832e0b58facfd3bb4db
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-04 10:37:10 -0500

    test setup cleanup

--- test/index.ts
+++ test/index.ts
@@ -2,35 +2,9 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import fs from "fs";
 import path from "path";
-import { StarknetVerifier } from '../typechain/StarknetVerifier';
-import { StarknetVerifier__factory } from "../typechain/factories/StarknetVerifier__factory";
-import { PedersenHash, PedersenHashNaive } from "../typechain";
+import { PedersenHash } from "../typechain";
 import {
-  pedersen as shiftedTablesPedersen,
-  precomputes as shiftedPrecomputes,
-} from "../lib/shifted_tables";
-
-function chunk<T>(arr: T[], len: number): T[][] {
-  const chunks: T[][] = [];
-  let i = 0;
-  const n = arr.length;
-
-  while (i < n) {
-    chunks.push(arr.slice(i, (i += len)));
-  }
-
-  return chunks;
-}
-
-import {
-  EventFilter,
-  Signer,
-  BigNumber,
   BigNumberish,
-  PopulatedTransaction,
-  BaseContract,
-  ContractTransaction,
-  CallOverrides,
 } from "ethers";
 
 interface MyBinaryProof {
@@ -55,7 +29,6 @@ interface MyContractData {
   contractStateRoot: BigNumberish;
   contractAddress: BigNumberish;
   storageVarAddress: BigNumberish;
-  storageVarValue: BigNumberish;
   classHash: BigNumberish;
   hashVersion: BigNumberish;
   nonce: BigNumberish;
@@ -93,15 +66,70 @@ function parseProofElement(element: any): MyStarknetProof {
   }
 }
 
+const toHex = (c: any) => `0x${c}`;
+
+const deployTables = async () => {
+  let precomputedContracts: any[64] = [];
+  for (const i in new Array(64).fill(0)) {
+    const generatedPath = path.join(__dirname, "..", "generated");
+
+    const bytecodePath = path.join(generatedPath, `${i}.bytecode`);
+    const bytecode = fs.readFileSync(bytecodePath);
+
+    const ContractCodePrecomputed = await ethers.getContractFactory(
+      "ContractCodePrecomputed"
+    );
+    const factory = new ethers.ContractFactory(
+      ContractCodePrecomputed.interface,
+      bytecode.toString(),
+      ContractCodePrecomputed.signer
+    );
+
+    console.log("Deploying contract: ", i);
+    const contract = await factory.deploy();
+    precomputedContracts.push(contract);
+    await contract.deployed();
+    console.log("Contract deployed: ", contract.address);
+  }
+  return precomputedContracts;
+}
+
+
 
 describe("Verify", function () {
+  let pedersenHash: any;
+  let proofverifier: any;
 
-  // it("Time test for a simple hash", async function () {
-  //   const PedersenFactory = await ethers.getContractFactory("PedersenHash");
-  //   const pedersen: PedersenHash = await PedersenFactory.attach("0x01cf58e264d7578D4C67022c58A24CbC4C4a304E");
-  //   const result = await pedersen.hash("0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37", "0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37");
-  //   console.log("Result: " + result);
-  // });
+  before(async () => {
+    const contracts = await deployTables();
+    try {
+      const PedersenHash = await ethers.getContractFactory("PedersenHash");
+      pedersenHash = await PedersenHash.deploy(
+        contracts.map((c: any) => c.address as string)
+      );
+      await pedersenHash.deployed();
+      console.log("PedersenHash contract has been deployed to: ", pedersenHash.address);
+
+      const StarknetVerifier = await ethers.getContractFactory("StarknetVerifier");
+      proofverifier = await StarknetVerifier.deploy(pedersenHash.address/*, starknetCoreContractAddress*/);
+      await proofverifier.deployed();
+      console.log("Verifier contract has been deployed to: ", proofverifier.address);
+    } catch (e) {
+      console.log(e)
+    }
+  });
+
+  it("Time test for a simple hash", async function () {
+    const PedersenFactory = await ethers.getContractFactory("PedersenHash");
+    const pedersen: PedersenHash = await PedersenFactory.attach("0x922D6956C99E12DFeB3224DEA977D0939758A1Fe");
+    const input = ethers.utils.defaultAbiCoder.encode(
+      ["uint256", "uint256"],
+      ["0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37", "0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37"]
+    );
+
+    const result = await pedersen.hash(input);
+    console.log("Result: " + result[0]);
+  });
 
   it("it should verify the proof", async function () {
     var jsonFile = "./sampleProof.json";
@@ -121,36 +149,25 @@ describe("Verify", function () {
       myStorageproofs.push(parseProofElement(element));
     });
 
-    // console.log("contract Proofs: " + myContractProofs.toString());
-    // console.log("storage Proofs: " + myStorageproofs.toString());
-
-    const StarknetVerifier = await ethers.getContractFactory("StarknetVerifier");
-
-    // UPDATE THIS ADDRESS WITH THE DEPLOYED ADDRESS OF PEDERSENHASH, Run the deploy script to get the address
-    const proofverifier = await StarknetVerifier.deploy("0x59C4e2c6a6dC27c259D6d067a039c831e1ff4947");
-    await proofverifier.deployed();
-
-    console.log("Deployed to: ", proofverifier.address);
     const starknetStateRoot = "0x1a5b65e4c309eb17b135fc9fcbf4201cf6c049fdf72c8180f0bb03c4d0eca37";
     const contractAddress = "0x6fbd460228d843b7fbef670ff15607bf72e19fa94de21e29811ada167b4ca39";
     const storageVarAddress = "0x0206F38F7E4F15E87567361213C28F235CCCDAA1D7FD34C9DB1DFE9489C6A091";
     const contractStateRoot = originalParse.result.contract_data.root;
-    const storageVarValue = "0x1e240";
+    const expectedStorageVarValue = "0x1e240";
 
     const contractData: MyContractData = {
       stateRoot: starknetStateRoot,
       contractStateRoot: contractStateRoot,
       contractAddress: contractAddress,
       storageVarAddress: storageVarAddress,
-      storageVarValue: storageVarValue,
       classHash: originalParse.result.contract_data.class_hash,
       hashVersion: originalParse.result.contract_data.contract_state_hash_version,
       nonce: originalParse.result.contract_data.nonce
     };
 
-    const result = await proofverifier.verifyCompleteProof(contractData
+    const result = await proofverifier.verifiedStorageValue(contractData
       , myContractProofs, myStorageproofs);
     console.log("Result: " + result);
-    expect(result).to.equal(true);
+    expect(result).to.equal(expectedStorageVarValue);
   });
 });
