aba623ea71d7e5a46a8772e68bc07fb5d428ac5c
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-05 18:13:38 -0500

    Add the new StarknetCoreContractStub containing only the methods we are interested in stateroot and blocknumber

---
+++ contracts/StarknetCoreContractStub.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.6.2 <0.9.0;
+
+contract StarknetCoreContractStub {
+    /**
+        Returns the current state root.
+        */
+    function stateRoot() external view returns (uint256) {
+        return
+            3321080970052968843805320357911538392446265524314601848332135665594593955343;
+    }
+
+    /**
+        Returns the current block number.
+        */
+    function stateBlockNumber() external view returns (int256) {
+        return 595364;
+    }
+}
