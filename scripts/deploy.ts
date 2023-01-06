ef9f3b887ce3cfab04edc7424761d3e2747bd391
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-05 19:26:03 -0500

    deployment updates to do conditional deployment on goerli vs mainnet vs local and pass in the appropriate starknetcorecontractaddress

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,3 @@
-import { expect } from "chai";
 import { ethers } from "hardhat";
 import fs from "fs";
 import * as dotenv from "dotenv";
@@ -33,35 +32,54 @@ const deployTables = async () => {
 }
 
 const deployAll = async () => {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const contracts = await deployTables();
 
   let pedersenHash: any;
-  try {
-    const PedersenHash = await ethers.getContractFactory("PedersenHash");
-    pedersenHash = await PedersenHash.deploy(
-      contracts.map((c: any) => c.address as string)
-    );
+  let proofverifier: any;
+  let starknetCoreContractStub: any;
 
-    await pedersenHash.deployed();
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+  try {
+
+    const StarknetVerifier = await ethers.getContractFactory("StarknetVerifier");
+    const network = await ethers.provider.getNetwork();
+    console.log("Network: ", network.name);
+    if (network.name == "goerli") {
+      console.log("Deploying to Goerli");
+      proofverifier = await StarknetVerifier.deploy('0x1a1eB562D2caB99959352E40a03B52C00ba7a5b1', '0xde29d060D45901Fb19ED6C6e959EB22d8626708e');
+    }
+    else if (network.name == "mainnet") {
+      console.log("Deploying to Mainnet");
+      // TODO: Add mainnet address
+      proofverifier = await StarknetVerifier.deploy('XXXXXXX', '0xc662c410C0ECf747543f5bA90660f6ABeBD9C8c4');
+    }
+    else {
+      console.log("Deploying to Localhost");
+      const contracts = await deployTables();
+
+      const PedersenHash = await ethers.getContractFactory("PedersenHash");
+      pedersenHash = await PedersenHash.deploy(
+        contracts.map((c: any) => c.address as string)
+      );
+      await pedersenHash.deployed();
+      console.log("PedersenHash contract has been deployed to: ", pedersenHash.address);
+
+      const StarknetCoreContractStub = await ethers.getContractFactory("StarknetCoreContractStub");
+      starknetCoreContractStub = await StarknetCoreContractStub.deploy();
+      await starknetCoreContractStub.deployed();
+      console.log("StarknetCoreContractStub contract has been deployed to: ", starknetCoreContractStub.address);
+
+      proofverifier = await StarknetVerifier.deploy(pedersenHash.address, starknetCoreContractStub.address);
+    }
+
+    await proofverifier.deployed();
+    console.log("Verifier contract has been deployed to: ", proofverifier.address);
+
   } catch (e) {
     console.log(e)
   }
-
-  console.log("PedersenHash contract has been deployed to: ", pedersenHash.address);
-  let proofverifier: any;
-  try {
-    const StarknetVerifier = await ethers.getContractFactory("StarknetVerifier");
-    proofverifier = await StarknetVerifier.deploy(pedersenHash.address/*, starknetCoreContractAddress*/);
-    await proofverifier.deployed();
-  } catch (e) {
-    console.log(e);
-  }
-  console.log("Verifier contract has been deployed to: ", proofverifier.address);
 }
 
 deployAll();
-//deployVerifier("0x922D6956C99E12DFeB3224DEA977D0939758A1Fe");
