e4077afa25fd6f40603f3b5db35a2f3bee4db525
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-07 09:26:36 -0500

    core contract stub for testing

--- contracts/StarknetCoreContractStub.sol
+++ contracts/StarknetCoreContractStub.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.6.2 <0.9.0;
 
+/// ONLY FOR TESTING PURPOSES TO SIMULATE STARKNET CORE CONTRACT
 contract StarknetCoreContractStub {
     /**
         Returns the current state root.
