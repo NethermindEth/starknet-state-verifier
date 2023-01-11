5008411ac0038fb64023051382a510c0a3c6fada
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-01-11 13:44:57 -0500

    temp revert to http until certs issue is resolved

--- frontend/src/utils/RpcCall.ts
+++ frontend/src/utils/RpcCall.ts
@@ -1,6 +1,6 @@
 
 // The URL of the JSON-RPC server
-const RPC_URL = 'https://139.144.170.180:9545/rpc/v0.2';
+const RPC_URL = 'http://139.144.170.180:9545/rpc/v0.2';
 
 // The JSON-RPC version
 const JSON_RPC_VERSION = '2.0';
