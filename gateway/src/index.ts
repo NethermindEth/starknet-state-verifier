e92eaaec13d4c633756e6f886c31fb4c13d201ad
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-02-16 10:50:50 -0500

    pass l2 pathfinder url via commandline

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -2,18 +2,16 @@ import { Server } from '@chainlink/ccip-read-server';
 import { Command } from 'commander';
 import { ethers } from 'ethers';
 import { hash, number } from "starknet";
-import { BigNumberish } from "ethers";
 import IStarknetResolver from './IStarknetResolverService.json'
 import StarknetoreContract from './StarknetCoreContract.json'
-import { parseStarknetProof, StarknetProof, StarknetCompositeStateProof, BinaryProof, EdgeProof, getStarknetProof } from 'pathfinder_getproof_lib';
+import { StarknetCompositeStateProof, getStarknetProof } from 'pathfinder_getproof_lib';
 
-const RPC_URL = 'https://pathfinder-goerli.nethermind.io/rpc/v0.2';
+const PATHFINDER_RPC_URL = 'https://pathfinder-goerli.nethermind.io/rpc/v0.2';
 const CORE_CONTRACT_ADDRESS = '0xde29d060D45901Fb19ED6C6e959EB22d8626708e'
 
 // const L2_RESOLVER_ADDRESS = '0x7412b9155cdb517c5d24e1c80f4af96f31f221151aab9a9a1b67f380a349ea3'
-
-// const namehash = require('eth-ens-namehash');
 // yarn start --l1_provider_url https://eth-goerli.alchemyapi.io/v2/ozZ0QV1qPAcGx_irBynNiiLkddGb689w  --l2_resolver_address 0x7412b9155cdb517c5d24e1c80f4af96f31f221151aab9a9a1b67f380a349ea3
+
 const computeStorageAddress = (name: string, keys: string[]) => {
   // storage_var address is the sn_keccak of the name hashed with the pedersen hash of the keys
   console.log(name, keys)
@@ -27,17 +25,16 @@ const computeStorageAddress = (name: string, keys: string[]) => {
 }
 
 
-
 const program = new Command();
 program
   .option('-l1p --l1_provider_url <url1>', 'L1_PROVIDER_URL', 'http://localhost:9545')
+  .option('-l2p --l2_provider_url <url2>', 'L2_PROVIDER_URL', PATHFINDER_RPC_URL)
   .option('-r --l2_resolver_address <address>', 'L2_PROVIDER_URL')
-  .option('-d --debug', 'debug', false)
   .option('-p --port <number>', 'Port number to serve on', '8081');
 program.parse(process.argv);
 const options = program.opts();
 console.log({ options })
-const { l1_provider_url, l2_resolver_address, debug } = options
+const { l1_provider_url, l2_provider_url, l2_resolver_address, debug } = options
 const l1_provider = new ethers.providers.JsonRpcProvider(l1_provider_url);
 
 const server = new Server();
@@ -53,7 +50,7 @@ server.add(IStarknetResolver.abi, [
         let starknetCoreContract = new ethers.Contract(CORE_CONTRACT_ADDRESS, StarknetoreContract.abi, l1_provider);
         const starknetCommittedBlockNumber = await starknetCoreContract.stateBlockNumber({ blockTag: blockNumber })
         const storageVarAddress = computeStorageAddress('resolver', [node])
-        myCompositeStateProof = await getStarknetProof(RPC_URL, l2_resolver_address, storageVarAddress, starknetCommittedBlockNumber)
+        myCompositeStateProof = await getStarknetProof(l2_provider_url, l2_resolver_address, storageVarAddress, starknetCommittedBlockNumber)
         console.log('myCompositeStateProof', JSON.parse(JSON.stringify(myCompositeStateProof)))
       } catch (e) {
         console.log(e)
