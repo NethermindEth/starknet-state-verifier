a8c077a3d8b13b7b664e3fbc97463329b446906d
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-03-22 10:15:56 -0400

    poseidon test

--- contracts/test/testpy.py
+++ contracts/test/testpy.py
@@ -10,15 +10,18 @@ from starkware.starknet.services.api.gateway.transaction import Deploy, InvokeFu
 from starkware.starknet.compiler.compile import compile_starknet_files, get_selector_from_name
 from starkware.cairo.lang.vm.crypto import pedersen_hash
 from starkware.starknet.public.abi import get_selector_from_name, get_storage_var_address, starknet_keccak
-
+from starkware.cairo.common.poseidon_hash import poseidon_hash
+ 
 #ROUGH TEST AREA, NOT PART OF THE PROJECT TESTS. USED FOR VERIFICATION, DEBUGGING AGAINST EXISTING PYTHON IMPLEMENTATION
 
-print("initializer ", get_storage_var_address('resolver'))
+
+print ("poseidon_hash", hex(poseidon_hash(1, 2)))
+# print("initializer ", get_storage_var_address('resolver'))
 # print("pedersen_hash ", hex(pedersen_hash(
 #     0x1e240, 0x206f38f7e4f15e87567361213c28f235cccdaa1d7fd34c9db1dfe9489c6a091) + 251))
 
-print("pedersen_hash state_hash ", hex(pedersen_hash(pedersen_hash(pedersen_hash(0x21a7f43387573b68666669a0ed764252ce5367708e696e31967764a90b429c2,
-      0x54d89b946081c6556161b34bcd7838c501575a3e9dc3d1d38a4438107b6b8fb), 0x0), 0x0)))
+# print("pedersen_hash state_hash ", hex(pedersen_hash(pedersen_hash(pedersen_hash(0x21a7f43387573b68666669a0ed764252ce5367708e696e31967764a90b429c2,
+#       0x54d89b946081c6556161b34bcd7838c501575a3e9dc3d1d38a4438107b6b8fb), 0x0), 0x0)))
 
 
 # print("pedersen_hash ", hex(pedersen_hash(
