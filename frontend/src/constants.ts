450210429ad4673d815f347cee7a2e1a1b29dec4
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-04-06 20:00:15 -0400

    updated deployment address for new verifier/resolver on goerli

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,5 +3,5 @@ export const L2_EXPLORER_BASE_URL = "https://testnet.starkscan.co/block";
 export const L2_RESOLVER_ADDRESS = '0x7412b9155cdb517c5d24e1c80f4af96f31f221151aab9a9a1b67f380a349ea3' // https://github.com/starknet-id/ens_resolver/blob/3577d3bf3e309614dbec16aca56b7cade2bac949/src/main.cairo#L7
 export const L2_RESOLVER_STORAGE_VAR_NAME = 'resolver'
 export const STARKNET_CORE_CONTRACT_ADDRESS = '0xde29d060D45901Fb19ED6C6e959EB22d8626708e'
-export const L1_RESOLVER_VERIFIER_ADDRESS = '0xBB49c34D4d92aC3207d589657fAC14186a470116'
+export const L1_RESOLVER_VERIFIER_ADDRESS = '0xF1979Ec7Fc554e83cF722978Cf73AE94381fBD92'
 export const PATHFINDER_RPC_URL = 'https://pathfinder-goerli.nethermind.io/rpc/v0.2';
